20e1ffcc671cd8ded8f5d1c5de4e4a9f14268226
GaragaNaveenKumar <23a35a0517@pragati.ac.in>
2025-12-28 21:17:24 +0530

    developed function to update order status from admin panel

--- AdminPanel/src/Pages/Orders/Orders.jsx
+++ AdminPanel/src/Pages/Orders/Orders.jsx
@@ -2,6 +2,7 @@ import React, { useState ,useEffect} from 'react';
 import "./Orders.css";
 import axios from "axios";
 import {toast} from "react-toastify";
+import {assets} from "../../assets/assets.js"
 
 const Orders = () => {
 
@@ -19,6 +20,18 @@ const Orders = () => {
     }
   }
 
+  const statusHandler=async (event,orderId)=>{
+    const response=await axios.post(apiUrl+"/api/order/status",{
+      orderId,
+      status:event.target.value
+    });
+    if(response.data.success){
+      await fetchAllOrders();
+    }
+
+
+  }
+
   useEffect(()=>{
     fetchAllOrders();
   },[])
@@ -27,8 +40,34 @@ const Orders = () => {
       <h3>Order page</h3>
       <div className="order-list">
         {orders.map((order,index)=>(
-          <div className='order-item'>
-            
+          <div key={index} className='order-item'>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="order-item-food">
+                {
+                  order.items.map((item,index)=>{
+                    if(index===order.items.length-1){
+                      return item.name+" x "+item.quantity;
+                    }else{
+                      return item.name+" x "+item.quantity+", ";
+                    }
+                  })
+                }
+              </p>
+              <p className='order-item-name'>{order.address.firstName+" "+order.address.lastName}</p>
+              <div className="order-item-address">
+                <p>{order.address.street+","}</p>
+                <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+              </div>
+              <p className='order-item-phone'>{order.address.phone}</p>
+            </div>
+            <p>Items: {order.items.length}</p>
+            <p className='price'>${order.amount}</p>
+            <select onChange={(event)=>statusHandler(event,order._id)} value={order.status} >
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
 
           </div>
         ))}
